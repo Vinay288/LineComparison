773d8d1a09e28744599260a4c22f9610cc5f76b5
Vinay288 <vinayhiremath288@gmail.com>
2021-08-25 12:26:49 +0530

    ADDED: method to calculate length while object creation

--- src/com/bridgelabz/linecomparison/Line.java
+++ src/com/bridgelabz/linecomparison/Line.java
@@ -1,5 +1,24 @@
 package com.bridgelabz.linecomparison;
 
 public class Line {
+	Float x1, x2, y1, y2, length;
+	private String name = "line";
+
+	Line(float x1, float x2, float y1, float y2) {
+		this.x1 = x1;
+		this.x2 = x2;
+		this.y1 = y1;
+		this.y2 = y2;
+		length = lengthOfLine();
+	}
+
+	Line(float x1, float x2, float y1, float y2, String name) {
+		this(x1, x2, y1, y2);
+		this.name = name;
+	}
+
+	public float lengthOfLine() {
+		return (float) Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+	}
 
 }
--- src/com/bridgelabz/linecomparison/LineComparison.java
+++ src/com/bridgelabz/linecomparison/LineComparison.java
@@ -3,8 +3,11 @@ package com.bridgelabz.linecomparison;
 public class LineComparison {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("********welcome to line comparison computation*********");
+		Line line1 = new Line(2,3,4,5);
+		Line line2 = new Line(4,6,7,8);
+		System.out.println("Length of line1 is "+line1.length);
+		System.out.println("Length of line 2 is "+line2.length);
 	}
 
 }
