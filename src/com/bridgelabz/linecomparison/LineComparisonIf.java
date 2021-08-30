0d5159d2d05116c5fd889c862c7c0166277c4b92
Vinay288 <vinayhiremath288@gmail.com>
2021-08-30 18:29:28 +0530

    ADDED:line comparison interface which has setlegth and comapre length methods

---
+++ src/com/bridgelabz/linecomparison/LineComparisonIf.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.linecomparison;
+
+public interface LineComparisonIf {
+	
+	public void equalityOfLines(Line line1, Line line2);
+
+	public void comapreLengthOfLines(Line line1, Line line2);
+
+}
